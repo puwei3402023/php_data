872f114691cab764aeee8235cf76083a9980a72e
蒲伟 <517421372@qq.com>
2018-03-07 22:24:28 +0800

    init

---
+++ src/GetDate.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2018/3/7
+ * Time: 21:59
+ */
+
+namespace pwdate;
+
+
+class GetDate
+{
+
+    public function index()
+    {
+        return 'hell';
+    }
+}
